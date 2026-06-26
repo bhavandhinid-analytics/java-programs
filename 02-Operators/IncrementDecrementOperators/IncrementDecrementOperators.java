d40c3e14cdedcc8576a10a57194ec8138040163f
Bhavandhini D <bhavandhinidurai2006@gmail.com>
2026-06-26 12:31:39 +0530

    Create IncrementDecrementOperators.java
    Add IncrementDecrementOperators program

---
+++ 02-Operators/IncrementDecrementOperators/IncrementDecrementOperators.java
@@ -0,0 +1,17 @@
+public class IncrementDecrementOperators {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Initial value: " + a);
+
+        System.out.println("Post Increment (a++): " + (a++));
+        System.out.println("Value after post increment: " + a);
+
+        System.out.println("Pre Increment (++a): " + (++a));
+
+        System.out.println("Post Decrement (a--): " + (a--));
+        System.out.println("Value after post decrement: " + a);
+
+        System.out.println("Pre Decrement (--a): " + (--a));
+    }
+}
