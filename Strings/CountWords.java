677a6abad3205cd0a8d3d2f10107ccd141af3c01
Bhavandhini D <bhavandhinidurai2006@gmail.com>
2026-07-01 13:50:58 +0530

    Create CountWords.java
    Added word counting program

---
+++ Strings/CountWords.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        sentence = sentence.trim();
+
+        if (sentence.isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = sentence.split("\\s+");
+            System.out.println("Number of words: " + words.length);
+        }
+
+        sc.close();
+    }
+}
+
+/*
+Sample Input:
+Java is easy to learn
+
+Output:
+Number of words: 5
+*/
